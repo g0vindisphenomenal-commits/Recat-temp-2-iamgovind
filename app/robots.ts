f3643db04b58c87348fa486ca85508657aa96755
g0vindisphenomenal-commits <g0vindisphenomenal@gmail.com>
2026-08-12 03:34:15 +0530

    Clean up robots.ts to produce minimal robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -9,10 +9,9 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/private/"],
       },
     ],
     sitemap: `${siteConfig.url}/sitemap.xml`,
-    host: siteConfig.url,
   };
 }
+
